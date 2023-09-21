9982f4ea007e0e4ceed73ddc682804fd7c8e7e19
sairevanth <sairevanth@vaultedge.com>
2023-09-21 17:33:44 +0530

    main file added

---
+++ sys-config-inspector/__main__.py
@@ -0,0 +1,12 @@
+from .inspector import SysConfigInspector
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="SysConfigInspector Command Line Tool")
+    parser.add_argument("--configuration-file", required=True, help="Path to the configuration file")
+    parser.add_argument("--report-location", required=True, help="Path to the report location")
+    args = parser.parse_args()
+    SysConfigInspector(configuration_file=args.configuration_file, report_location=args.report_location)
+    
+if __name__ == "__main__":
+    main()
