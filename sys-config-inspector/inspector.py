45ed853405a4adf2f5f74e636c0ed594dfe9fea4
sairevanth <sairevanth@vaultedge.com>
2023-09-22 06:14:58 +0530

    nargs added in parser

--- sys-config-inspector/inspector.py
+++ sys-config-inspector/inspector.py
@@ -178,7 +178,7 @@ class SysConfigInspector():
 
 def main():
     parser = argparse.ArgumentParser(description="SysConfigInspector Command Line Tool")
-    parser.add_argument("--configuration-file", required=True, help="Path to the configuration file")
+    parser.add_argument("--configuration-file", nargs='+', required=True, help="Path to the configuration file")
     parser.add_argument("--report-location", required=True, help="Path to the report location")
     args = parser.parse_args()
     SysConfigInspector(configuration_file=args.configuration_file, report_location=args.report_location)
