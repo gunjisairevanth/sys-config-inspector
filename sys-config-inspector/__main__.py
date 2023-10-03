793badde197ce7fa3a71ea12515232801e5e902b
sairevanth <sairevanth@vaultedge.com>
2023-10-03 15:28:27 +0530

    --env-values-files argu added

--- sys-config-inspector/__main__.py
+++ sys-config-inspector/__main__.py
@@ -5,6 +5,7 @@ def main():
     parser = argparse.ArgumentParser(description="SysConfigInspector Command Line Tool")
     parser.add_argument("--configuration-file", nargs='+', required=True, help="Path to the configuration file")
     parser.add_argument("--report-location", required=True, help="Path to the report location")
+    parser.add_argument("--env-values-files", required=True, help="Path to the report location")
     args = parser.parse_args()
     SysConfigInspector(configuration_file=args.configuration_file, report_location=args.report_location)
     
