e6320a94077b8b23f312d2005ec7db95cdba62a9
sairevanth <sairevanth@vaultedge.com>
2023-10-03 15:32:42 +0530

    env-values-files added to main

--- sys-config-inspector/__main__.py
+++ sys-config-inspector/__main__.py
@@ -7,7 +7,7 @@ def main():
     parser.add_argument("--report-location", required=True, help="Path to the report location")
     parser.add_argument("--env-values-files", required=True, help="Path to the report location")
     args = parser.parse_args()
-    SysConfigInspector(configuration_file=args.configuration_file, report_location=args.report_location)
+    SysConfigInspector(configuration_file=args.configuration_file, report_location=args.report_location,env_values_files=args.env_values_files)
     
 if __name__ == "__main__":
     main()
