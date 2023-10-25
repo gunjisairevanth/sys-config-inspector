5220e3e921160821f599d2d73cd1633f48377fb3
sairevanth <sairevanth@vaultedge.com>
2023-10-25 17:24:44 +0530

    version updated

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.4'
+VERSION = '0.0.1'
 DESCRIPTION = 'This package helps to do the basic sysem check and generate html report'
 
 # Setting up
